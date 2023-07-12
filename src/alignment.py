d45f2309c979b28245720ff8708dd0ba5af79fb4
Parker S <parker.senter@gmail.com>
2023-07-12 18:41:53 +0000

    got alignments working

--- src/alignment.py
+++
@@ -1,5 +0,0 @@
-class Alignment:
-    def __init__(self, alignment):
-        def self.alignment = alignment
-
-    
--- src/tests/test_iteration1.py
+++ src/tests/test_iteration1.py
@@ -1,4 +1,5 @@
 from character import Character
+from morals import Alignment 
 
 def test_get_name():
     c = Character("Bill")
@@ -7,9 +8,38 @@ def test_get_name():
 def test_set_name():
     d = Character("Bob")
     d.set_name("Bob")
-    assert d.name == 'Bob'
+    assert d.name == "Bob"
 
 def test_set_name2():
     d = Character("Joe")
     d.set_name("Joe")
     assert d.name == "Joe"
+
+
+def test_get_alignment():
+    a = Alignment("Good")
+    assert a.alignment == "Good"
+
+def test_get_alignment():
+    a = Alignment("Neutral")
+    assert a.alignment == "Neutral"
+
+def test_get_alignment():
+    a = Alignment("Evil")
+    assert a.alignment == "Evil"
+
+
+def test_set_alignment():
+    a = Alignment("Evil")
+    a.set_alignment("Evil")
+    assert a.alignment == "Evil"
+
+def test_set_alignment():
+    a = Alignment("Neutral")
+    a.set_alignment("Neutral")
+    assert a.alignment == "Neutral"
+
+def test_set_alignment():
+    a = Alignment("Good")
+    a.set_alignment("Good")
+    assert a.alignment == "Good"
