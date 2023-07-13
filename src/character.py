5214f7cb09caa6f8ef03f1352d282a5d15de9bd4
Parker S <parker.senter@gmail.com>
2023-07-13 18:31:13 +0000

    experience started working

--- src/character.py
+++ src/character.py
@@ -7,6 +7,8 @@ class Character:
         self.alignment = "Neutral"
         self.armor_class = 10 
         self.hit_points = 5
+        self.experience_points = 0
+        self.level = 1
         self.abilities = {
             "strength": 10,
             "dexterity": 10,
@@ -35,31 +37,39 @@ class Character:
     #Armor and HP
 
     def get_armor_class(self):
-        return self.armor_class
+        dexterity_modifier = self.get_dexterity_modifier()
+        return self.armor_class + dexterity_modifier
 
     def set_armor_class(self, armor_class):
         self.armor_class = armor_class
 
     def get_hit_points(self):
-        return self.hit_points
+        constitution_modifier = self.get_constitution_modifier()
+        return max(1, self.hit_points + constitution_modifier)
 
     def set_hit_points(self, hit_points):
-        self.hit_points = hit_points
+        self.hit_points = max(1, hit_points)
 
     #-------------------------------------
     #Attack
 
-    def attack(self,roll, enemy):
+    def attack(self, roll, enemy):
         if roll == 20:
+            self.gain_experience(10)
             return True
-        return roll >= enemy.get_armor_class()
+        attack_roll = roll + self.get_strength_modifier()
+        if attack_roll >= enemy.get_armor_class():
+            self.gain_experience(10)
+            return True
+        return False
 
     #-------------------------------------
     #Damage
     
     def damage_taken(self, damage):
         if damage > 0:
-            self.hit_points -= damage
+            modified_damage = max(1, damage + self.get_strength_modifier())
+            self.hit_points -= modified_damage
         if self.hit_points <= 0:
             self.hit_points = 0
 
@@ -69,20 +79,49 @@ class Character:
     #-------------------------------------
     #Abilities
 
-    def get_strength(self, ability):
-        return self.abilities[ability]
+    def get_strength(self):
+        return self.abilities["strength"]
 
-    def get_dexterity(self, ability):
-        return self.abilities[ability]
+    def get_dexterity(self):
+        return self.abilities["dexterity"]
 
-    def get_constitution(self, ability):
-        return self.abilities[ability]
+    def get_constitution(self):
+        return self.abilities["constitution"]
 
-    def get_wisdom(self, ability):
-        return self.abilities[ability]
+    def get_wisdom(self):
+        return self.abilities["wisdom"]
 
-    def get_intellegence(self, ability):
-        return self.abilities[ability]
+    def get_intellegence(self):
+        return self.abilities["intellegence"]
 
-    def get_charisma(self, ability):
-        return self.abilities[ability]
+    def get_charisma(self):
+        return self.abilities["charisma"]
+
+    def set_ability_score(self, ability, score):
+        if ability in self.abilities:
+            self.abilities[ability] = score
+
+    def change_ability_score(self, ability, modifier):
+        if ability in self.abilities:
+            self.abilities[ability] += modifier
+
+    def get_strength_modifier(self):
+        strength = self.get_strength()
+        modifier = (strength - 10) // 2
+        return modifier
+
+    def get_dexterity_modifier(self):
+        dexterity = self.get_dexterity()
+        modifier = (dexterity - 10) // 2
+        return modifier
+
+    def get_constitution_modifier(self):
+        constitution = self.get_constitution()
+        modifier = (constitution - 10) // 2
+        return modifier
+
+    #-------------------------------------
+    #Experience and levels
+    
+    def gain_experience(self, points):
+        self.experience_points += points
--- src/tests/test_iteration1.py
+++ src/tests/test_iteration1.py
@@ -45,6 +45,13 @@ def test_set_hit_points():
     d.set_hit_points(8)
     assert d.hit_points == 8
 
+def test_hit_points_minimum_value():
+    character = Character()
+    character.set_hit_points(0)
+    character.set_ability_score("constitution", 8)
+
+    assert character.get_hit_points() == 1
+
 #-------------------------------------
 #Attack
 
@@ -64,6 +71,15 @@ def test_cannot_attack():
     enemy.set_armor_class(11)
     assert ourcharacter.attack(10, enemy) == False
 
+def test_can_attack_with_modifier():
+    ourcharacter = Character()
+    ourcharacter.set_name("Attacker")
+    ourcharacter.set_ability_score("strength", 14)
+    enemy = Character()
+    enemy.set_name("Defender")
+    enemy.set_armor_class(9)
+    assert ourcharacter.attack(10, enemy) == True
+
 #-------------------------------------
 #Damage
 
@@ -81,14 +97,73 @@ def test_is_alive():
     character = Character()
     assert character.is_alive() == True
 
+def test_damage_taken_with_modifier():
+    ourcharacter = Character()
+    ourcharacter.set_name("Attacker")
+    ourcharacter.set_ability_score("strength", 14)
+
+    enemy = Character()
+    enemy.set_name("Defender")
+    enemy.set_hit_points(15)
+
+    enemyhitpoints = enemy.get_hit_points()
+    damage = 10 + ourcharacter.get_strength_modifier()
+    enemy.damage_taken(damage)
+    assert enemy.get_hit_points() == enemyhitpoints - max(1, damage)
+
+
 #-------------------------------------
 #Abilities
 
 def test_ability():
     character = Character()
-    assert character.get_strength("strength") == 10
-    assert character.get_dexterity("dexterity") == 10
-    assert character.get_constitution("constitution") == 10
-    assert character.get_wisdom("wisdom") == 10
-    assert character.get_intellegence("intellegence") == 10
-    assert character.get_charisma("charisma") == 10
+    assert character.get_strength() == 10
+    assert character.get_dexterity() == 10
+    assert character.get_constitution() == 10
+    assert character.get_wisdom() == 10
+    assert character.get_intellegence() == 10
+    assert character.get_charisma() == 10
+
+def test_set_ability_score():
+    character = Character()
+    character.set_ability_score("strength", 12)
+    assert character.get_strength() == 12
+
+def test_change_ability_score():
+    character = Character()
+    character.set_ability_score("dexterity", 10)
+    character.change_ability_score("dexterity", 0)
+    assert character.get_dexterity() == 10
+
+def test_ability_with_modifier():
+    character = Character()
+    character.set_ability_score("strength", 14)
+    assert character.get_strength() == 14
+    assert character.get_strength_modifier() == 2
+
+def test_armor_class_with_dexterity_modifier():
+    character = Character()
+    character.set_armor_class(10)
+    character.set_ability_score("dexterity", 14)
+    assert character.get_armor_class() == 12
+
+def test_hit_points_with_constitution_modifier():
+    character = Character()
+    character.set_hit_points(5)
+    character.set_ability_score("constitution", 12)
+
+    assert character.get_hit_points() == 6
+
+#-------------------------------------
+#Experience and levels
+
+def test_gain_experience():
+    Attacker = Character()
+    Attacker.set_name("Parker")
+    Defender = Character()
+    Defender.set_name("Mike_Tyson")
+    Defender.set_armor_class(9)
+    starting_xp = Attacker.experience_points
+    Attacker.attack(10, Defender)
+    assert Attacker.experience_points == starting_xp + 10
+
