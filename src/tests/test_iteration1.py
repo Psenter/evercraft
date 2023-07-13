47214d282ad198c806ed0930302b3ce050ecb69d
Parker S <parker.senter@gmail.com>
2023-07-13 13:48:05 +0000

    Changed layout of classes

--- src/tests/test_iteration1.py
+++ src/tests/test_iteration1.py
@@ -1,15 +1,78 @@
 from character import Character
 
+#-------------------------------------
+#Name
+
 def test_get_name():
-    c = Character("Bill")
-    assert c.name == "Bill"
+    d = Character()
+    assert d.name == "NightMan"
 
 def test_set_name():
-    d = Character("Bob")
+    d = Character()
     d.set_name("Bob")
-    assert d.name == 'Bob'
+    assert d.name == "Bob"
 
-def test_set_name2():
-    d = Character("Joe")
-    d.set_name("Joe")
-    assert d.name == "Joe"
+#-------------------------------------
+#Alignment
+
+def test_get_alignment():
+    d = Character()
+    assert d.alignment == "Neutral"
+
+def test_set_alignment():
+    d = Character()
+    d.set_alignment("Good")
+    assert d.alignment == "Good"
+
+#-------------------------------------
+#Armor and HP
+
+def test_get_armor_class():
+    d = Character()
+    assert d.armor_class == 10
+
+def test_set_armor_class():
+    d = Character()
+    d.set_armor_class(9)
+    assert d.armor_class == 9
+
+def test_get_hit_points():
+    d = Character()
+    assert d.hit_points == 5
+
+def test_set_hit_points():
+    d = Character()
+    d.set_hit_points(8)
+    assert d.hit_points == 8
+
+#-------------------------------------
+#Attack
+
+def test_can_attack():
+    ourcharacter = Character()
+    ourcharacter.set_name("Attacker")
+    enemy = Character()
+    enemy.set_name("Defender")
+    enemy.set_armor_class(9)
+    assert ourcharacter.attack(10, enemy) == True
+
+def test_cannot_attack():
+    ourcharacter = Character()
+    ourcharacter.set_name("Attacker")
+    enemy = Character()
+    enemy.set_name("Defender")
+    enemy.set_armor_class(11)
+    assert ourcharacter.attack(10, enemy) == False
+
+#-------------------------------------
+#Damage
+
+def test_damage_taken():
+     ourcharacter = Character()
+     ourcharacter.set_name("Attacker")
+     enemy = Character()
+     enemy.set_name("Defender")
+     enemyhitpoints = enemy.get_hit_points()
+     if ourcharacter.attack(10, enemy):
+        enemy.damage_taken(10)
+     assert enemy.get_hit_points() < enemyhitpoints
